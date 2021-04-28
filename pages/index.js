ce528599cbdd3ec28dc5690a6bbe7c489693842d
Souravdey777 <piyush.kolkata@gmail.com>
2021-04-28 17:08:30 +0530

    🚧 nav links fixed

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 // import Link from "next/link";
 import About from "../components/about";
-import { ROUTES } from "../utils/routes";
-import Clog from "@souravdey/colored-console";
 import { SOCIAL_LINKS } from "../utils/links";
 import { Link, animateScroll as scroll } from "react-scroll";
+import WorkExperience from "../components/workExperience";
 
 const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
 const currentTime = () => {
@@ -18,8 +17,6 @@ const currentTime = () => {
 };
 
 export default function Home() {
-  // const myRef = useRef(null);
-  // const executeScroll = () => scrollToRef(myRef);
   const [time, setTime] = useState(currentTime());
 
   useEffect(() => {
@@ -61,7 +58,7 @@ export default function Home() {
         />
       </Head>
       <main className={styles.main}>
-        <div className="LogoNav" style={{ fontWeight: "bold" }}>
+        <div className="LogoNav" style={{ fontWeight: "bolder" }}>
           <span className="gradientText">Sourav Dey</span>
         </div>
         {/* Social Media Links in desktop */}
@@ -100,136 +97,26 @@ export default function Home() {
               /workExperience
             </Link>
           </div>
+          <div>
+            <Link
+              activeClass="active"
+              to="techSkills"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              /techSkills
+            </Link>
+          </div>
         </div>
         <div className="body">
           <div>
             <About />
-            <div style={{ marginTop: "3rem" }}>
-              <Image src="/images/dots.png" width={190} height={64} />
-            </div>
-            <p
-              id="workExperience"
-              style={{
-                marginTop: "3rem",
-                textAlign: "right",
-                fontWeight: "bold",
-              }}
-            >
-              /workExperience
-            </p>
-            <p
-              style={{
-                marginTop: "3rem",
-                fontWeight: "bold",
-                fontSize: "0.75rem",
-              }}
-            >
-              git checkout <span className="gradientText">WORK EXPERIENCE</span>
-            </p>
-            <p
-              className="gradientText"
-              style={{
-                marginTop: "3rem",
-                fontSize: "1rem",
-                fontWeight: "bold",
-              }}
-            >
-              <span className="gradientText">TATA CONSULTANCY SERVICES</span>
-            </p>
-            <div style={{ fontSize: "1rem" }}>
-              <span className="gradientText">Full Stack Developer</span>
-            </div>
-            Sept 2018 - Present
-            <br />
-            <br />
-            <p>
-              Worked in multiple Projects and Technologies while gaining
-              Proficiency in HTML, CSS, JavaScript, React.js & React Native,
-              Express.js Node.js.
-              <br />
-              <br />
-              Proficient with concepts of Single Page Applications, Progressive
-              Web Applications, Rest APIs, and Google Cloud.
-              <br />
-              <br />
-              Created User Stories, developed highly impactful app both mobile
-              and web with that also contributed to Product marketing strategies
-              and UX designing
-              <br />
-              <br />
-              Mentored Full-Stack Development Internship for undergrad students.
-              <br />
-              <br />
-              <b className="gradientText">Awards</b>
-              <br />
-              - Special Initiative Award (For Developing TCS CLICKFIT)
-              <br />
-              - CSR Special Initiative Award (For Mentoring an Internship)
-              <br />- Special Initiative Award
-              <br />- Applause for Team Award
-              <br />- On the Spot Award (For Developing AIA INSURANCE WEB
-              PORTAL)
-              <br />- Fresco Play Learning Award
-            </p>
-            {/* <div className="projectTest">this is test</div> */}
-            <p
-              className="gradientText"
-              style={{
-                marginTop: "3rem",
-                fontSize: "1rem",
-                fontWeight: "bold",
-              }}
-            >
-              <span className="gradientText">
-                INDIAN OIL CORPORATION LIMITED (IOCL)
-              </span>
-            </p>
-            <div style={{ fontSize: "1rem" }}>
-              <span className="gradientText">Software Developer Intern</span>
-            </div>
-            Jul 2017 – Aug 2017
-            <br />
-            <br />
-            <p>
-              Gained experience with Database Modeling, Understanding of SAP
-              software, and Network Schema of IOCL.
-              <br />
-              <br />
-              INVENTORY MANAGEMENT SYSTEM | JAVA (SWING), SQL
-              <br />A System Software for Managing Inventory, Stocks, Sales,
-              Purchase, and Reports
-            </p>
-            <p
-              className="gradientText"
-              style={{
-                marginTop: "3rem",
-                fontSize: "1rem",
-                fontWeight: "bold",
-              }}
-            >
-              <span className="gradientText">HEWLETT-PACKARD (HP)</span>
-            </p>
-            <div style={{ fontSize: "1rem" }}>
-              <span className="gradientText">Android Developer Trainee</span>
-            </div>
-            Jul 2016 – Aug 2016
-            <br />
-            <br />
-            <p>
-              Skill-building Experience in Java SE & Android while completing
-              various assignments and projects.
-            </p>
-            <div
-              style={{
-                marginTop: "3rem",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <Image src="/images/dots.png" width={190} height={64} />
-              <div className="animatedCircle"></div>
-            </div>
+            <WorkExperience/>
+            
             {/* techSkills */}
+            <div id="techSkills">
             <p
               style={{
                 marginTop: "3rem",
@@ -259,13 +146,16 @@ export default function Home() {
               }}
             >
               <div className="skill">
-                <span style={{ padding: "0rem 0.5rem" }}>HTML</span>
+                <span style={{ padding: "0rem 0.5rem" }}>HTML5</span>
                 {/* <span style={{ padding: "0rem 0.5rem" }}>+</span>
                 <span style={{ padding: "0rem 0.5rem" }}>|</span>
                 <span style={{ padding: "0rem 0.5rem" }}>12</span> */}
               </div>
               <div className="skill">
-                <span style={{ padding: "0rem 0.5rem" }}>CSS</span>
+                <span style={{ padding: "0rem 0.5rem" }}>CSS3</span>
+              </div>
+              <div className="skill">
+                <span style={{ padding: "0rem 0.5rem" }}>JavaScript</span>
               </div>
               <div className="skill">
                 <span style={{ padding: "0rem 0.5rem" }}>React.js</span>
@@ -277,15 +167,29 @@ export default function Home() {
                 <span style={{ padding: "0rem 0.5rem" }}>Node.js</span>
               </div>
               <div className="skill">
+                <span style={{ padding: "0rem 0.5rem" }}>Express.js</span>
+              </div>
+              <div className="skill">
+                <span style={{ padding: "0rem 0.5rem" }}>MongoDB</span>
+              </div>
+              <div className="skill">
+                <span style={{ padding: "0rem 0.5rem" }}>SQL</span>
+              </div>
+              <div className="skill">
+                <span style={{ padding: "0rem 0.5rem" }}>Java</span>
+              </div>
+              <div className="skill">
                 <span style={{ padding: "0rem 0.5rem" }}>firebase</span>
               </div>
               <div className="skill">
                 <span style={{ padding: "0rem 0.5rem" }}>Google CLoud</span>
               </div>
+              
+            </div>
             </div>
             <div
               style={{
-                marginTop: "5rem",
+                marginTop: "30rem",
                 fontWeight: "bold",
                 display: "flex",
                 justifyContent: "space-between",

e5a99f24cc8bc22382876cb6d35eb361cb49b6f6
Souravdey777 <piyush.kolkata@gmail.com>
2023-06-18 01:23:58 +0530

    script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import { Navigation, Footer } from "@/components";
 import "./globals.css";
 import styles from "@/app/page.module.css";
 import { poppins } from "./fonts";
+import Script from "next/script";
 // import { motion } from "framer-motion";
 // import Animate from "./animation";
 
 export const metadata = {
   title: "Sourav Dey",
-  description: "Sourav Dey: Portfolio Website",
+  description: "Sourav Dey: Digital Space",
   // themeColor: [
   //   { media: "(prefers-color-scheme: light)", color: "cyan" },
   //   { media: "(prefers-color-scheme: dark)", color: "black" },
@@ -21,6 +22,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BJ96LSKFFB"
+        strategy="lazyOnload"
+      />
       <body className={poppins.className}>
         <header>
           <strong className={styles.fixedHeader}>Sourav Dey</strong>
